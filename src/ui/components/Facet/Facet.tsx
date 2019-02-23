014693d7697248456d1b58ad520c6d4ac8cb6765
Linden Dale-Gandar <linden.dale-gandar@synergia.co.nz>
2019-02-23 23:43:50 +1300

    Refactored Facet to functional component with state hook

--- src/ui/components/Facet/Facet.tsx
+++ src/ui/components/Facet/Facet.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { FC, useState } from "react";
 import classnames from "classnames";
 import styles from "./Facet.module.scss";
 import { ReactComponent as Chevron } from "../../../assets/icons/chevron-up.svg";
@@ -12,76 +12,66 @@ type Props = {
     value: number;
   }[];
   type: string;
-  filterFacet: (bucketKey: string | null, facetKey: string, facetType: string) => void;
-  checkBucket: (key: string, filter: []) => boolean;
+  filterFacet: (
+    bucketKey: string | null,
+    facetKey: string,
+    facetType: string
+  ) => void;
+  checkBucket: (key: string, filters: []) => boolean;
   filters: [];
 };
 
-type State = {
-  collapse: boolean;
-};
+const Facet: FC<Props> = ({
+  facetKey,
+  buckets,
+  type,
+  checkBucket,
+  filters,
+  filterFacet
+}) => {
+  const [collapse, toggleCollapse] = useState(false);
 
-class Facet extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      collapse: false
-    };
-  }
-
-  toggleCollapse() {
-    this.setState({ collapse: !this.state.collapse });
-  }
-
-  toggleBucket(bucket: string | null) {
-    const { filterFacet, facetKey, type } = this.props;
-    filterFacet(bucket, facetKey, type);
-  }
-
-  resetFilter(){
-    this.toggleBucket(null)
-  }
-
-  render() {
-    const { facetKey, buckets, type, checkBucket, filters } = this.props;
-    const { collapse } = this.state;
-
-    return (
-      <div
-        key={facetKey}
-        className={classnames(styles.facet, { [styles.collapse]: collapse })}
-      >
-        <div className={styles.title} >
+  return (
+    <div
+      key={facetKey}
+      className={classnames(styles.facet, { [styles.collapse]: collapse })}
+    >
+      <div className={styles.title}>
+        <div className={styles.flex}>
+          <div>{facetKey}</div>
           <div className={styles.flex}>
-            <div>{facetKey}</div>
-            <div className={styles.flex}>
-              {filters.length>0 &&
-              <div className={styles.filter} onClick={() => this.resetFilter()}>
+            {filters.length > 0 && (
+              <div
+                className={styles.filter}
+                onClick={() => filterFacet(null, facetKey, type)}
+              >
                 <ClearFilter />
-              </div>}
-
-              <div className={styles.chevron} onClick={() => this.toggleCollapse()}>
-                <Chevron />
               </div>
-            </div>
+            )}
 
+            <div
+              className={styles.chevron}
+              onClick={() => toggleCollapse(!collapse)}
+            >
+              <Chevron />
+            </div>
           </div>
         </div>
-        <div className={styles["bucket-list"]}>
-          {buckets.map(bucket => (
-            <Bucket
-              key={`${facetKey}-${bucket.key}`}
-              name={bucket.key}
-              count={bucket.value}
-              toggleBucket={(name: string) => this.toggleBucket(name)}
-              type={type}
-              active={checkBucket(bucket.key, filters)}
-            />
-          ))}
-        </div>
       </div>
-    );
-  }
-}
+      <div className={styles["bucket-list"]}>
+        {buckets.map(bucket => (
+          <Bucket
+            key={`${facetKey}-${bucket.key}`}
+            name={bucket.key}
+            count={bucket.value}
+            toggleBucket={(name: string) => filterFacet(name, facetKey, type)}
+            type={type}
+            active={checkBucket(bucket.key, filters)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Facet;
