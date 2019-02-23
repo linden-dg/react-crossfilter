f64f8e0a21e3e67efa287ff0d210874a8239f8f1
Linden Dale-Gandar <linden.dale-gandar@synergia.co.nz>
2019-02-24 00:03:40 +1300

    Refactored Bucket to FC

--- src/ui/components/Facet/Bucket.tsx
+++ src/ui/components/Facet/Bucket.tsx
@@ -1,37 +1,29 @@
-import React, { Component } from "react";
+import React, { FC } from "react";
 import classnames from "classnames";
 import styles from "./Bucket.module.scss";
 import { ReactComponent as CloseIcon } from "../../../assets/icons/close.svg";
 
 type Props = {
   name: string;
+  toggleBucket: (bucketKey: string) => void;
   count?: number;
-  toggleBucket: (bucketKey: string, e: {}) => void;
-  active: boolean;
+  active?: boolean;
   type?: string;
 };
 
-class Bucket extends Component<Props> {
-  render() {
-    const { name, count, toggleBucket, active, type } = this.props;
-
-    return (
-      <div
-        onClick={e => toggleBucket(name, e)}
-        className={classnames(styles.bucket, { [styles.active]: active })}
-      >
-        <div className={styles.label}>
-          {type === "composite" ? JSON.parse(name) : name}&nbsp;
-        </div>
-        <div className={styles.count}>({count})</div>
-        {/*{active && (*/}
-        <div className={styles.close}>
-          <CloseIcon />
-        </div>
-        {/*)}*/}
-      </div>
-    );
-  }
-}
+const Bucket: FC<Props> = ({ name, toggleBucket, count, active, type }) => (
+  <div
+    onClick={() => toggleBucket(name)}
+    className={classnames(styles.bucket, { [styles.active]: active })}
+  >
+    <div className={styles.label}>
+      {type === "composite" ? JSON.parse(name) : name}
+    </div>
+    {count && <div className={styles.count}>({count})</div>}
+    <div className={styles.close}>
+      <CloseIcon />
+    </div>
+  </div>
+);
 
 export default Bucket;
